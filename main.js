5e1aa8f7abbb8edb4cdbc4f9acb65a6cd22e38eb
ahmadammmoura <ammoura575@gmail.com>
2021-03-07 13:19:05 +0200

    done

---
+++ main.js
@@ -0,0 +1,23 @@
+var device = prompt("you want mobile or laptop")
+
+
+
+while(device !== "mobile" && device !== "laptop"){
+    device = prompt("you want mobile or laptop")
+}
+
+
+var item = ''
+
+if(device == "laptop" || device == "mobile"){
+    item = item+device
+}
+
+var numberOfItems = prompt(`how many ${device}s you want` )
+
+var img = `<img src = "/img/${device}.jpg" width = "150px"  height ="150px"   />` 
+
+for(var i = 1 ; i<=numberOfItems ; i++){
+    document.write(img)
+}
+
