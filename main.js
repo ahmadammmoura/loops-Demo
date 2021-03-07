7e4bc7a6f4d97a1037dc6765f91b857c28a80bb7
ahmadammmoura <55942495+ahmadammmoura@users.noreply.github.com>
2021-03-07 13:50:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ if(device == "laptop" || device == "mobile"){
 
 var numberOfItems = prompt(`how many ${device}s you want` )
 
-var img = `<img src = "https://ahmadammmoura.github.io/loops-Demo/${device}.jpg" width = "150px"  height ="150px"   />` 
+var img = `<img src = "img/${device}.jpg" width = "150px"  height ="150px"   />` 
 
 for(var i = 1 ; i<=numberOfItems ; i++){
     document.write(img)
